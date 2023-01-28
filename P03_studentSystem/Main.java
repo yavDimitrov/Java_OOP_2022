281e00ac181991e6637c3fad7ff41d90e4b2b11f
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-01-28 23:52:04 +0200

    03

---
+++ P03_studentSystem/Main.java
@@ -0,0 +1,21 @@
+package L03_Working_Abstraction.P03_studentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StudentSystem studentSystem = new StudentSystem();
+
+        while (true) {
+            String[] input = scanner.nextLine().split("\\s+");
+
+            if (input[0].equals("Exit")){
+                break;
+            }
+
+            studentSystem.ParseCommand(input);
+        }
+    }
+}
---
+++ P03_studentSystem/Student.java
@@ -0,0 +1,51 @@
+package L03_Working_Abstraction.P03_studentSystem;
+
+public class Student {
+    private String name;
+    private int age;
+    private double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+   /* public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGrade() {
+        return this.grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }*/
+
+    public String getInfo() {
+        String studentInfo = String.format("%s is %s years old.", name, age);
+
+        if (grade >= 5.00) {
+            studentInfo += " Excellent student.";
+        } else if (grade < 5.00 && grade >= 3.50) {
+            studentInfo += " Average student.";
+        } else {
+            studentInfo += " Very nice person.";
+        }
+
+        return studentInfo;
+    }
+}
