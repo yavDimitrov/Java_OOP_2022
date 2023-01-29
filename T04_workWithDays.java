5fa98bc929f74fd5101c7aa114b660a87b141b48
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-01-29 22:25:54 +0200

    02

---
+++ T04_workWithDays.java
@@ -0,0 +1,10 @@
+package L03_Working_Abstraction;
+
+public class T04_workWithDays {
+    public static void main(String[] args) {
+
+        DayOfWeek dayOfWeek = DayOfWeek.TUESDAY;
+
+        System.out.println(dayOfWeek.getDayNumber());
+    }
+}
