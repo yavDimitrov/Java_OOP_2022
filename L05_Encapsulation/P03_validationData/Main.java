39eac3dbf0a82e41b66121ee27dcc82c1632bc5b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-02-04 23:44:15 +0200

    P03

---
+++ L05_Encapsulation/P03_validationData/Main.java
@@ -0,0 +1,41 @@
+package L05_Encapsulation.P03_validationData;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner reader = new Scanner(System.in);
+        int numberOfPeople = Integer.parseInt(reader.nextLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            String[] input = reader.nextLine().split(" ");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+
+            Person personToAdd = new Person(firstName, lastName , age, salary);
+            people.add(personToAdd);
+        }
+
+        /*  if (comparisonResult != 0) {
+                return comparisonResult;
+            } else {
+                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+            }   */
+
+        double bonus = Double.parseDouble(reader.nextLine());
+
+        for(Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person.toString());
+        }
+    }
+}
